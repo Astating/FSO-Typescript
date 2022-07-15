b1ad7b67786c260508248d9f7b611bd34925e021
Constant G <constant.guilbert@polyconseil.fr>
2022-07-16 00:39:06 +0200

    Handling errors and exceptions [functions calculateBmi & calculateExercises]

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -2,12 +2,41 @@ function calculateBmi(heightInCentimeters: number, weight: number): string {
     const heightInMeters = heightInCentimeters / 100;
     const bmi = weight / (heightInMeters**2);
 
-    if (bmi < 18.5) return "Underweight";
-    if (bmi > 18.5 && bmi < 25) return "Normal";
-    if (bmi > 25) return "Overweight";
+    const bmiDescription: string = (() => {
+        if (bmi < 18.5) return "Underweight";
+        if (bmi > 18.5 && bmi < 25) return "Normal";
+        if (bmi > 25) return "Overweight";
+    })();
+
+    return `BMI: ${bmi.toFixed(2)} => ${bmiDescription}`;
 }
 
-const height: number = Number(process.argv[2]);
-const weight: number = Number(process.argv[3]);
+interface BmiParams {
+    height: number;
+    weight: number;
+}
 
-console.log(calculateBmi(height, weight));
+const parseBmiArguments = (args: Array<string>): BmiParams => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+  
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+      return {
+        height: Number(args[2]),
+        weight: Number(args[3])
+      }
+    } else {
+      throw new Error('Provided values were not numbers!');
+    }
+}
+
+try {
+    const { height, weight } = parseBmiArguments(process.argv);
+    console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,8 +1,8 @@
 function calculateExercises(dailyExerciseHours: Array<number>, dailyTarget: number) {
     const periodLength: number = dailyExerciseHours.length;
 
-    const trainingDays: number = dailyExerciseHours.reduce((totalDays, hours) => {
-        return hours > 0 ? totalDays + 1 : totalDays;
+    const trainingDays: number = dailyExerciseHours.reduce((totalDays, trainingHours) => {
+        return trainingHours > 0 ? totalDays + 1 : totalDays;
     }, 0);
 
     const average: number = dailyExerciseHours.reduce((total, hours) => total + hours, 0) / periodLength;
@@ -31,16 +31,42 @@ function calculateExercises(dailyExerciseHours: Array<number>, dailyTarget: numb
         }
     })();
 
-    return {periodLength, trainingDays, success, rating, ratingDescription, target: dailyTarget, average };
+    return { periodLength, trainingDays, success, rating, ratingDescription, target: dailyTarget, average };
 }
 
-const target: number = Number(process.argv[2]);
-const hours: number[] = (() => {
-    const processedArg = [];
-    for (let i = 3 ; i < process.argv.length ; i++) {
-        processedArg.push(Number(process.argv[i]));
-    }
-    return processedArg;
-})();
+interface ExerciseValues {
+    dailyHours: number[];
+    target: number;
+}
 
-console.log(calculateExercises(hours, target));
+const parseArguments = (args: Array<string>): ExerciseValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+
+    const hours: number[] = (() => {
+        const processedArg = [];
+        for (let i = 3 ; i < process.argv.length ; i++) {
+            processedArg.push(Number(process.argv[i]));
+        }
+        return processedArg;
+    })();
+
+    if (!isNaN(Number(args[2])) && !hours.some((arg) => isNaN(Number(arg)))) {
+        return {
+          target: Number(args[2]),
+          dailyHours: hours
+        }
+      } else {
+        throw new Error('Provided values were not numbers!');
+      }
+}
+
+try {
+    const { target, dailyHours } = parseArguments(process.argv);
+    console.log(calculateExercises(dailyHours, target));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
