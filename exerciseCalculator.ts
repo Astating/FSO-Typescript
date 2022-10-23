d2b971b3a6731585fab8acbdca78ddf5fd6ae7c4
Constant G <constant.guilbert@polyconseil.fr>
2022-10-23 13:23:27 +0200

    9.7: POST /calculate => respond with appropriate error or successful result

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,72 +1,90 @@
-function calculateExercises(dailyExerciseHours: Array<number>, dailyTarget: number) {
-    const periodLength: number = dailyExerciseHours.length;
+export function calculateExercises(
+  dailyExerciseHours: Array<number>,
+  dailyTarget: number
+) {
+  const periodLength: number = dailyExerciseHours.length;
 
-    const trainingDays: number = dailyExerciseHours.reduce((totalDays, trainingHours) => {
-        return trainingHours > 0 ? totalDays + 1 : totalDays;
-    }, 0);
+  const trainingDays: number = dailyExerciseHours.reduce(
+    (totalDays, trainingHours) => {
+      return trainingHours > 0 ? totalDays + 1 : totalDays;
+    },
+    0
+  );
 
-    const average: number = dailyExerciseHours.reduce((total, hours) => total + hours, 0) / periodLength;
+  const average: number =
+    dailyExerciseHours.reduce((total, hours) => total + hours, 0) /
+    periodLength;
 
-    const success: boolean = average >= dailyTarget;
+  const success: boolean = average >= dailyTarget;
 
-    const rating: 1 | 2 | 3 = (() => {
-        if (success) {return 3;}
-        else if (average > dailyTarget / 2) {return 2;}
-        else return 1;
-    })();
+  const rating: 1 | 2 | 3 = (() => {
+    if (success) {
+      return 3;
+    } else if (average > dailyTarget / 2) {
+      return 2;
+    } else return 1;
+  })();
 
-    const ratingDescription: string = (() => {
-        switch(rating) {
-            case 1:
-                return "Meh. You can still do it next time!";
-                break;
-            case 2:
-                return "Eh, you were not that far from your goal!";
-                break;
-            case 3:
-                return "Success!";
-                break;
-            default:
-                return "Hmm, this should not happen";
-        }
-    })();
+  const ratingDescription: string = (() => {
+    switch (rating) {
+      case 1:
+        return "Meh. You can still do it next time!";
+        break;
+      case 2:
+        return "Eh, you were not that far from your goal!";
+        break;
+      case 3:
+        return "Success!";
+        break;
+      default:
+        return "Hmm, this should not happen";
+    }
+  })();
 
-    return { periodLength, trainingDays, success, rating, ratingDescription, target: dailyTarget, average };
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target: dailyTarget,
+    average,
+  };
 }
 
 interface ExerciseValues {
-    dailyHours: number[];
-    target: number;
+  dailyHours: number[];
+  target: number;
 }
 
 const parseArguments = (args: Array<string>): ExerciseValues => {
-    if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length < 4) throw new Error("Not enough arguments");
 
-    const hours: number[] = (() => {
-        const processedArg = [];
-        for (let i = 3 ; i < process.argv.length ; i++) {
-            processedArg.push(Number(process.argv[i]));
-        }
-        return processedArg;
-    })();
+  const hours: number[] = (() => {
+    const processedArg = [];
+    for (let i = 3; i < process.argv.length; i++) {
+      processedArg.push(Number(process.argv[i]));
+    }
+    return processedArg;
+  })();
 
-    if (!isNaN(Number(args[2])) && !hours.some((arg) => isNaN(Number(arg)))) {
-        return {
-          target: Number(args[2]),
-          dailyHours: hours
-        };
-      } else {
-        throw new Error('Provided values were not numbers!');
-      }
+  if (!isNaN(Number(args[2])) && !hours.some((arg) => isNaN(Number(arg)))) {
+    return {
+      target: Number(args[2]),
+      dailyHours: hours,
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
 };
 
 try {
-    const { target, dailyHours } = parseArguments(process.argv);
-    console.log(calculateExercises(dailyHours, target));
+  const { target, dailyHours } = parseArguments(process.argv);
+  console.log(calculateExercises(dailyHours, target));
 } catch (error: unknown) {
-    let errorMessage = 'Something bad happened.';
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    console.log(errorMessage);
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
 }
--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { calculateBmi } from "./bmiCalculator";
+import { calculateExercises } from "./exerciseCalculator";
 
 const app = express();
+app.use(express.json());
 
 app.get("/hello", (_req, res) => {
   res.send("Hello, world !");
@@ -14,6 +16,28 @@ app.get("/bmi", (req, res) => {
     res.status(400).send({ error: "Malformatted parameters" });
   }
   res.send(calculateBmi(...args));
+});
+
+app.post("/calculate", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises, target } = req.body;
+
+  if (daily_exercises === undefined || target === undefined) {
+    res.status(400).send({ error: "Parameters missing." });
+  }
+  if (
+    !Array.isArray(daily_exercises) ||
+    daily_exercises.some((value) => isNaN(Number(value))) ||
+    isNaN(Number(target))
+  ) {
+    res.status(400).send({ error: "Malformatted parameters." });
+  }
+
+  const calculated = calculateExercises(
+    (daily_exercises as number[]).map(Number), // Actually, true => 1, which is not desirable. Better to use Number.parseInt (beware that Number.parseInt can take multiple parameters so you should not use it like this ".map(Number.parseInt)" but like this ".map(v => Number.parseInt(v)")
+    Number(target)
+  );
+  res.send(calculated);
 });
 
 const PORT = 3003;
